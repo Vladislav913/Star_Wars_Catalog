d79ad920849689a91a21f7cdf36e99cada8e6db9
Vladislav_913 <you@example.com>
2023-01-02 18:54:17 +0200

    add beforeunload event

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import {useEffect} from "react";
 import CardList from "./Component/CardList/CardList";
 import Header from "./Component/Header/Header";
 import "./App.css"
 
 function App() {
+
+    var isOnIOS = navigator.userAgent.match(/iPad/i)|| navigator.userAgent.match(/iPhone/i);
+var eventName = isOnIOS ? "pagehide" : "beforeunload";
+
+useEffect(() => {
+    window.addEventListener(eventName, function (event) { 
+        if(!isOnIOS){
+            event.returnValue = true;
+            return
+        }
+        window.event.cancelBubble = true; // Don't know if this works on iOS but it might!
+        
+    });
+}, [eventName, isOnIOS])
+
+
     return (
         <div className="App">
             <Header/>
